26e24b3fdf10ac0a03c220eb7a23261f588a8baa
thiago-aranha <aranha1981@gmail.com>
2024-05-22 08:09:20 -0300

    Creating server

---
+++ server.js
@@ -0,0 +1,12 @@
+import http from "http";
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain"});
+    res.end("Node.js training");
+});
+
+server.listen(port, () => {
+    console.log("Server listening");
+})
