021f0d863c7a06a5d2510754b5490ca82a16a3d9
thiago-aranha <aranha1981@gmail.com>
2024-05-29 08:55:33 -0300

    Removing files added by mistake

--- src/cfg/dbconnect.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-async function connectLibraryDatabase() {
-    mongoose.connect("mongodb+srv://admin:admin@cluster0.ixdwmfq.mongodb.net/library?retryWrites=true&w=majority&appName=Cluster0");
-    return mongoose.connection;
-};
-
-export default connectLibraryDatabase;
