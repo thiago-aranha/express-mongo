6fe8fb09995093fca4f99e3be9c460e00c550b27
thiago-aranha <aranha1981@gmail.com>
2024-05-29 10:40:24 -0300

    Creating environment variables for sensible data

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+import "dotenv/config";
 import app from "./src/app.js";
 
-const port = 3000;
+const port = process.env.SERVER_PORT;
 
 app.listen(port, () => {
     console.log("Server listening");
