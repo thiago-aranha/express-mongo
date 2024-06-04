d661076eab76ae5029f7de6d1afbe543cbf20b84
thiago-aranha <aranha1981@gmail.com>
2024-06-04 10:52:07 -0300

    Implementing error handler for books controller

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -2,60 +2,60 @@ import book from "../models/bookModel.js";
 import { author } from "../models/authorModel.js";
 
 class BookController {
-    static async getBooks (req, res) {
+    static async getBooks (req, res, next) {
         try {
             const bookList = await book.find({});
             res.status(200).json(bookList);
         } catch (error) {
-            res.status(500).json({message: `${error.message} - Error getting book list`});
+            next(error);
         }            
     };
     
-    static async getBookById (req, res) {
+    static async getBookById (req, res, next) {
         try {
             const searchedBook = await book.findById(req.params.id);
             res.status(200).json(searchedBook);
         } catch (error) {
-            res.status(500).json({message: `${error.message} - Failed to obtain book with id ${req.params.id}`})
+            next(error);
         }
     };
 
-    static async getBooksByCompany (req, res) {
+    static async getBooksByCompany (req, res, next) {
         const searchedCompany = req.query.company;
         try {
             const booksByCompany = await book.find({ company: searchedCompany});
             res.status(200).json(booksByCompany);
         } catch (error) {
-            res.status(500).json({message: `${error.message} - Failed to obtain books from company ${searchedCompany}`})
+            next(error);
         }
     }
     
-    static async addBook (req, res) {
+    static async addBook (req, res, next) {
         try {
             const searchedAuthor = await author.findById(req.body.author);
             const bookWithAuthor = { ...req.body, author: { ...searchedAuthor._doc} }
             const newBook = await book.create(bookWithAuthor);
             res.status(201).json({ message: "Book successfully created!", book: newBook});
         } catch (error) {
-            res.status(500).json({message: `${error.message} - Error to create book`});
+            next(error);
         }
     };
     
-    static async updateBook (req, res) {
+    static async updateBook (req, res, next) {
         try {
             await book.findByIdAndUpdate(req.params.id, req.body);
             res.status(200).json({message: `Book ${req.params.id} successfully updated!`});
         } catch (error) {
-            res.status(500).json({message: `${error.message} - Failed to update book with id ${req.params.id}`});
+            next(error);
         }
     };
 
-    static async deleteBook (req, res) {
+    static async deleteBook (req, res, next) {
         try {
             await book.findByIdAndDelete(req.params.id);
             res.status(200).json({message: `Book ${req.params.id} successfully deleted!`});
         } catch (error) {
-            res.status(500).json({message: `${error.message} - Failed to remove book with id ${req.params.id}`});
+            next(error);
         }
     };
 
--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -3,6 +3,10 @@ import mongoose from "mongoose";
 function errorHandler(error, req, res, next) {
     if (error instanceof mongoose.Error.CastError) {
         res.status(400).json({message: `Invalid request format ${req.params.id}`});
+    } else if (error instanceof mongoose.Error.ValidationError) {
+        res.status(400).send({message: `Validation error: ${Object.values(error.errors)
+            .map(error => error.message)
+            .join("; ")}`})
     } else {
         res.status(500).json({message: 'Internal error'});
     }
