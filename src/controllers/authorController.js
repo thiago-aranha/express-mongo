324ce10bb123c6cba121ca45380f3b132a0e520e
thiago-aranha <aranha1981@gmail.com>
2024-06-04 17:07:22 -0300

    Evolving error return on delete and update author

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -35,8 +35,12 @@ class AuthorController {
     
     static async updateAuthor (req, res, next) {
         try {
-            await author.findByIdAndUpdate(req.params.id, req.body);
-            res.status(200).json({message: `Author ${req.params.id} successfully updated!`});
+            const updatedAuthor = await author.findByIdAndUpdate(req.params.id, req.body);
+            if (updatedAuthor !== null) {
+                res.status(200).json({message: `Author ${req.params.id} successfully updated!`});
+            } else {
+                res.status(404).json({message: `Author ID not found ${req.params.id}`});
+            }
         } catch (error) {
             next(error);
         }
@@ -44,8 +48,12 @@ class AuthorController {
 
     static async deleteAuthor (req, res, next) {
         try {
-            await author.findByIdAndDelete(req.params.id);
-            res.status(200).json({message: `Author ${req.params.id} successfully deleted!`});
+            const deletedAuthor = await author.findByIdAndDelete(req.params.id);
+            if (deletedAuthor !== null) {
+                res.status(200).json({message: `Author ${req.params.id} successfully deleted!`});
+            } else {
+                res.status(404).json({message: `Author ID not found ${req.params.id}`});
+            }
         } catch (error) {
             next(error);
         }
